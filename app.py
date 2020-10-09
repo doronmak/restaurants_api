3d8d23fa0b0d30e42ede03cd23ba8aea30ce51da
Doron Makmel <makmel.info@gmail.com>
2020-10-09 23:59:52 +0300

    post request

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from database.db_manager import DB
 from restaurants_analyzer import CSVAnalyzer
+from pydantic import BaseModel
 import config
 
 app = FastAPI()
@@ -10,6 +11,13 @@ restaurants_db = DB(db_type=config.db_type, db_user=config.db_user, db_password=
                     db_name=config.db_name)
 
 csv_reader = CSVAnalyzer(config.Restaurant_file)
+
+
+class Restaurant(BaseModel):
+    Name: str
+    Type: str
+    Phone: str
+    Location: list
 
 
 def first_app_init():
@@ -32,8 +40,10 @@ def get_all_restaurant():
 
 
 @app.post('/restaurant')
-def post_restaurant():
-    pass
+async def post_restaurant(restaurant: Restaurant):
+    restaurants_db.add_new_restaurant(rest_name=restaurant.Name, rest_type=restaurant.Type, rest_phone=restaurant.Phone,
+                                      rest_location=restaurant.Location)
+    return "post new restaurant"
 
 
 @app.get('/restaurants/{rest_id}')
@@ -48,4 +58,5 @@ def patch_restaurant_by_id(rest_id):
 
 @app.delete('/restaurants/{rest_id}')
 def delete_restaurant_by_id(rest_id):
-    return restaurants_db.delete_restaurant_by_id(rest_id)
+    restaurants_db.delete_restaurant_by_id(rest_id)
+    return "delete restaurant"
